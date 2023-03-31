9750c45df6830237309fa5afac0a6246872145c7
Sohini Bhattacharjee <sohinismail@gmail.com>
2023-03-31 15:42:24 +0530

    Added some styling on the interface

--- netflix-ui/src/components/BackgroundImage.jsx
+++ netflix-ui/src/components/BackgroundImage.jsx
@@ -3,14 +3,15 @@ import styled from "styled-components";
 import background from "../assets/login.jpg";
 
 export default function BackgroundImage() {
-    return (
-        <Container>
-            <img src={background} alt="background" />
-        </Container>
-    );
+  return (
+    <Container>
+      <img src={background} alt="background" />
+    </Container>
+  );
 }
 
 const Container = styled.div`
+  background-repeat: no-repeat;
   height: 100vh;
   width: 100vw;
   img {
--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -5,25 +5,29 @@ import Header from "../components/Header";
 
 export default function Signup() {
     return (
-        <Container>
+        <Container >
             <BackgroundImage />
-            <Header />
+
             <div className="content">
+                <Header login />
+
                 <div className="body flex column a-center j-center">
                     <div className="text flex column">
-                        <h1>Unlimited TV Shows, Movies and more!</h1>
+                        <h1>Unlimited Movies, TV Shows and more!</h1>
                         <h4>Watch anywhere. Cancel anytime</h4>
                         <h6>Ready to watch? Enter your email to create or restart your membership</h6>
                     </div>
-                </div>
-                <div>
+
                     <div className="form">
                         <input type="email" placeholder="Email Address" name="email" />
                         <input type="password" placeholder="Password" name="password" />
                         <button>Get Started</button>
 
                     </div>
+
                     <button>Log in</button>
+
+
                 </div>
 
             </div>
@@ -36,12 +40,13 @@ const Container = styled.div`
 position: relative; 
 .content{
     position: absolute;
-height: 100vh;
-width: 100vw;
-top:0;
-left:0;
-background-color: rgba(0,0,0,0.5);
-display: grid;
+    top:0;
+    left:0;
+    background-color: rgba(0,0,0,0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows:15vh 85vh;
 
 }
 
